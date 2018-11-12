7d1d645e179a17ff50fb37d90c17b899736a3d4e
David Grayston <davidgrayston@googlemail.com>
2018-11-12 23:28:10 +0000

    Data provider for testHooverRoomRequest

--- tests/Feature/HooverRoomRequestTest.php
+++ tests/Feature/HooverRoomRequestTest.php
@@ -10,25 +10,54 @@ class HooverRoomRequestTest extends TestCase
     /**
      * Test input/output of /api/room/hoover endpoint.
      *
-     * @return void
+     * @dataProvider hooverRoomRequestProvider
      */
-    public function testHooverRoomRequest()
+    public function testHooverRoomRequest($input, $output)
     {
-        $response = $this->postJson('/api/room/hoover', [
-          'roomSize' => [5, 5],
-          'coords' => [1, 2],
-          'patches' => [
-            [1, 0],
-            [2, 2],
-            [2, 3],
-          ],
-          'instructions' => 'NNESEESWNWW',
-        ]);
-
+        $response = $this->postJson('/api/room/hoover', $input);
         $response->assertStatus(201);
-        $response->assertExactJson([
-          'coords' => [1, 3],
-          'patches' => 1,
-        ]);
+        $response->assertExactJson($output);
+    }
+
+    /**
+     * Data provider for hoover requests.
+     */
+    public function hooverRoomRequestProvider()
+    {
+        return [
+          [
+            [
+              'roomSize' => [5, 5],
+              'coords' => [1, 2],
+              'patches' => [
+                [1, 0],
+                [2, 2],
+                [2, 3],
+              ],
+              'instructions' => 'NNESEESWNWW',
+            ],
+            [
+              'coords' => [1, 3],
+              'patches' => 1,
+            ],
+          ],
+          [
+            [
+              'roomSize' => [2, 2],
+              'coords' => [0, 0],
+              'patches' => [
+                [0, 0],
+                [0, 1],
+                [1, 1],
+                [1, 0],
+              ],
+              'instructions' => 'NESW',
+            ],
+            [
+              'coords' => [0, 0],
+              'patches' => 4,
+            ],
+          ],
+        ];
     }
 }
