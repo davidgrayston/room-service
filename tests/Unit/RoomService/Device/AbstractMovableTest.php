94cc6491ec2d017ad725c96b384ecdeee3931806
David Grayston <davidgrayston@googlemail.com>
2018-11-15 22:58:05 +0000

    Testing getArea() exception

--- tests/Unit/RoomService/Device/AbstractMovableTest.php
+++ tests/Unit/RoomService/Device/AbstractMovableTest.php
@@ -14,6 +14,17 @@ use Tests\TestCase;
  */
 class AbstractMovableTest extends TestCase
 {
+    /**
+     * @var \App\RoomService\Device\AbstractMovable
+     */
+    protected $movable;
+
+    /**
+     * Setup test objects.
+     */
+    public function setup() {
+        $this->movable = $this->getMockForAbstractClass(AbstractMovable::class);
+    }
 
     /**
      * @covers ::setArea
@@ -21,10 +32,18 @@ class AbstractMovableTest extends TestCase
      */
     public function testSetArea()
     {
-        $movable = $this->getMockForAbstractClass(AbstractMovable::class);
         $area = new Area([1, 2]);
-        $movable->setArea($area);
-        $this->assertEquals($area, $movable->getArea());
+        $this->movable->setArea($area);
+        $this->assertEquals($area, $this->movable->getArea());
     }
 
+    /**
+     * @covers ::getArea
+     * @expectedException LogicException
+     * @expectedExceptionMessage Item does not have an area set.
+     */
+    public function testGetAreaException()
+    {
+        $this->movable->getArea();
+    }
 }
